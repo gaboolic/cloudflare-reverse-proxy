b2f67d11e0f9c4cb882ad19feb02d61d0b80e2e6
gaboolic <23441099@qq.com>
2023-06-03 12:51:23 +0000

    init

---
+++ worker.js
@@ -0,0 +1,26 @@
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+  })
+  
+  async function handleRequest(request) {
+    const url = new URL(request.url);
+    const actualUrlStr = url.pathname.replace("/proxy/","")
+    console.log(actualUrlStr)
+  
+    const actualUrl = new URL(actualUrlStr)
+  
+    const modifiedRequest = new Request(actualUrl, {
+      headers: request.headers,
+      method: request.method,
+      body: request.body,
+      redirect: 'follow'
+    });
+  
+    const response = await fetch(modifiedRequest);
+    const modifiedResponse = new Response(response.body, response);
+  
+    // 添加允许跨域访问的响应头
+    modifiedResponse.headers.set('Access-Control-Allow-Origin', '*');
+  
+    return modifiedResponse;
+  }
